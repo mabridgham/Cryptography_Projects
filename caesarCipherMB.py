cfcb5525b0eb3404fd7335513802632673024d7d
unknown <bridgham.ma@gmail.com>
2021-10-16 13:54:58 -0700

    simplified greeting code

--- caesarCipherMB.py
+++ caesarCipherMB.py
@@ -8,13 +8,13 @@ import pyperclip
 
 # Print greeting:
 
-print('Named after Julius Caesar, the Caesar Cipher is one of the oldest ciphers and is based on the')
-print('simplest monoalphabetic cipher. A Caesar cipher is categorized as a substitution cipher')
-print('in which the alphabet in the plain text is shifted by a fixed number down the alphabet.\n')
-print('While the original Caesar Cipher only uses only letters, this program adds symbols to the')
-print('list as well as letters allowing for not only more complex messages but a longer and stronger keyspace.\n')
-print('To use this program, Enter the message you wish to either encrypt or decrypt. Then choose the value')
-print('of the shift by choosing a number between 1 and 95.\n')
+print("""Named after Julius Caesar, the Caesar Cipher is one of the oldest ciphers and is based on the
+simplest monoalphabetic cipher. A Caesar cipher is categorized as a substitution cipher
+in which the alphabet in the plain text is shifted by a fixed number down the alphabet.\n
+While the original Caesar Cipher only uses only letters, this program adds symbols to the
+list as well as letters allowing for not only more complex messages but a longer and stronger keyspace.
+To use this program, Enter the message you wish to either encrypt or decrypt. Then choose the value
+of the shift by choosing a number between 1 and 95.\n""")
 
 
 # The string to be encrypted/decrypted:
@@ -32,6 +32,7 @@ mode = 'encrypt' # Set to either 'encrypt' or 'decrypt'
 # Every possible symbol that can be encrypted:
 
 SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz[\\]^_`{|}~ !"#$%&\'()*+,-./0123456789:;<=>?@'
+
 
 # Store the encrypted/decrypted form of the message:
 
@@ -70,3 +71,6 @@ for symbol in message:
 
 print(translated)
 pyperclip.copy(translated)
+
+
+
