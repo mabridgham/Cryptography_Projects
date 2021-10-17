660641b932391494d416edbd2cb1c0ad1e8b263b
unknown <bridgham.ma@gmail.com>
2021-10-17 16:29:02 -0700

    added description to comments

--- cryptoMathMB.py
+++ cryptoMathMB.py
@@ -1,14 +1,24 @@
 # Cryptomath Module
 # https://www.nostarch.com/crackingcodes (BSD Licensed)
+# Finds the Greates Common Divisor of two numbers a and b. Also finds the
+# Modular Inverse of two numbers.
+# In mathematics, the Euclidean algorithm, or Euclid's algorithm, is an efficient 
+# method for computing the greatest common divisor (GCD) of two integers (numbers),
+# the largest number that divides them both without a remainder.
+# To learn more about the Extended Euclidean Algorithm visit and why it is important 
+# to cryptography https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
 
 def gcd(a, b):
+    
     # Return the Greatest Common Divisor of a and b using Euclid's Algorithm
+    
     while a != 0:
         a, b = b % a, a
     return b
 
 
 def findModInverse(a, m):
+    
     # Return the modular inverse of a % m, which is
     # the number x such that a*x % m = 1
 
@@ -16,6 +26,7 @@ def findModInverse(a, m):
         return None # No mod inverse exists if a & m aren't relatively prime.
 
     # Calculate using the Extended Euclidean Algorithm:
+    
     u1, u2, u3 = 1, 0, a
     v1, v2, v3 = 0, 1, m
     while v3 != 0:
