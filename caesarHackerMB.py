dd30ba49e22e91702bd134e8a8dcacc00ba32b93
unknown <bridgham.ma@gmail.com>
2021-10-17 15:57:24 -0700

    add greeting

--- caesarHackerMB.py
+++ caesarHackerMB.py
@@ -4,6 +4,15 @@
 # simplest monoalphabetic cipher. A Caesar cipher is categorized as a substitution cipher
 # in which the alphabet in the plain text is shifted by a fixed number down the alphabet.
 
+# Print greeting:
+
+print("""Named after Julius Caesar, the Caesar Cipher is one of the oldest ciphers and is based on the
+simplest monoalphabetic cipher. A Caesar cipher is categorized as a substitution cipher
+in which the alphabet in the plain text is shifted by a fixed number down the alphabet.\n
+To use this program, enter a message encrypted with the Caesar Cipher program. This program
+tests and displays the results for every possible shift value. Scroll down the list until
+you see a translated message.\n""")
+
 # Get message from user:
 
 message = input('Enter message to be decoded: ')
