77c707b2e4cccd657c16cc7e2b0386319e5642c5
unknown <bridgham.ma@gmail.com>
2021-10-18 14:25:49 -0700

    added description and formatting

--- freqAnalysisMB.py
+++ freqAnalysisMB.py
@@ -1,12 +1,18 @@
 # Frequency Finder
-# https://www.nostarch.com/crackingcodes (BSD Licensed)
+# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+# In crytanalysis, frequency analysis is the study of the frequency of letters
+# or groups of letters in a ciphertext. The method is used as an aid to breaking
+# classical ciphers.
 
 ETAOIN = 'ETAOINSHRDLCUMWFGYPBVKJXQZ'
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
+
 def getLetterCount(message):
+
     # Returns a dictionary with keys of single letters and values of the
     # count of how many times they appear in the message parameter:
+
     letterCount = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L': 0, 'M': 0, 'N': 0, 'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0}
 
     for letter in message.upper():
@@ -17,18 +23,22 @@ def getLetterCount(message):
 
 
 def getItemAtIndexZero(items):
+
     return items[0]
 
 
 def getFrequencyOrder(message):
+
     # Returns a string of the alphabet letters arranged in order of most
-    # frequently occurring in the message parameter.
+    # frequently occuring in the message parameter.
 
     # First, get a dictionary of each letter and its frequency count:
+
     letterToFreq = getLetterCount(message)
 
-    # Second, make a dictionary of each frequency count to each letter(s)
+    # Second, make a dictionary of each frequency count to the letter(s)
     # with that frequency:
+
     freqToLetter = {}
     for letter in LETTERS:
         if letterToFreq[letter] not in freqToLetter:
@@ -38,17 +48,20 @@ def getFrequencyOrder(message):
 
     # Third, put each list of letters in reverse "ETAOIN" order, and then
     # convert it to a string:
+
     for freq in freqToLetter:
         freqToLetter[freq].sort(key=ETAOIN.find, reverse=True)
         freqToLetter[freq] = ''.join(freqToLetter[freq])
 
     # Fourth, convert the freqToLetter dictionary to a list of
-    # tuple pairs (key, value), then sort them:
+    # tuple pairs (key, value), and then sort them:
+
     freqPairs = list(freqToLetter.items())
     freqPairs.sort(key=getItemAtIndexZero, reverse=True)
 
     # Fifth, now that the letters are ordered by frequency, extract all
     # the letters for the final string:
+
     freqOrder = []
     for freqPair in freqPairs:
         freqOrder.append(freqPair[1])
@@ -57,21 +70,27 @@ def getFrequencyOrder(message):
 
 
 def englishFreqMatchScore(message):
+
     # Return the number of matches that the string in the message
     # parameter has when its letter frequency is compared to English
     # letter frequency. A "match" is how many of its six most frequent
-    # and six least frequent letters is among the six most frequent and
+    # and six least frequent letters are among the six most frequent and
     # six least frequent letters for English.
+
     freqOrder = getFrequencyOrder(message)
 
     matchScore = 0
+
     # Find how many matches for the six most common letters there are:
+
     for commonLetter in ETAOIN[:6]:
         if commonLetter in freqOrder[:6]:
             matchScore += 1
+
     # Find how many matches for the six least common letters there are:
-    for uncommonLetter in ETAOIN[-6:]:
-        if uncommonLetter in freqOrder[-6:]:
+
+    for uncommonLetter in ETAOIN[:-6]:
+        if uncommonLetter in freqOrder[:-6]:
             matchScore += 1
 
     return matchScore
