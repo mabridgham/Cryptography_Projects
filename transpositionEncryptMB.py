8ced85d208fbc26ba28c6a1d2502288e849bf3e9
unknown <bridgham.ma@gmail.com>
2021-10-18 15:06:58 -0700

    added description and formatting

--- transpositionEncryptMB.py
+++ transpositionEncryptMB.py
@@ -1,45 +1,57 @@
 # Transposition Cipher Encryption
-# https://www.nostarch.com/crackingcodes (BSD Licensed)
+# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+# A Transposition Cipher is a cipher in which the letters of the plaintext
+# are systematically rearranged into another sequence.
 
 import pyperclip
 
 def main():
+
     myMessage = '''Augusta Ada King-Noel, Countess of Lovelace (10 December 1815 - 27 November 1852) was an English mathematician and writer, chiefly known for her work on Charles Babbage's early mechanical general-purpose computer, the Analytical Engine. Her notes on the engine include what is recognised as the first algorithm intended to be carried out by a machine. As a result, she is often regarded as the first computer programmer.'''
     myKey = 13
 
     ciphertext = encryptMessage(myKey, myMessage)
 
-    # Print the encrypted string in ciphertext to the screen, with
+    # Print the encrypted string in ciphertext to the screen with
     # a | ("pipe" character) after it in case there are spaces at
-    # the end of the encrypted message.
+    # the end of the encrypted message:
+
     print(ciphertext + '|')
 
-    # Copy the encrypted string in ciphertext to the clipboard.
+    # Copy the encrypted string in cipertext to the clipboard:
+
     pyperclip.copy(ciphertext)
 
-
 def encryptMessage(key, message):
-    # Each string in ciphertext represents a column in the grid.
+
+    # Each string in ciphertext represents a column in the grid:
+
     ciphertext = [''] * key
 
-    # Loop through each column in ciphertext.
+    # Loop through each column in ciphertext:
+
     for column in range(key):
         currentIndex = column
 
-        # Keep looping until currentIndex goes past the message length.
+        # Keep looping until currentIndex goes past the message length:
+
         while currentIndex < len(message):
+
             # Place the character at currentIndex in message at the
-            # end of the current column in the ciphertext list.
+            # end of the current column in the ciphertext list:
+
             ciphertext[column] += message[currentIndex]
 
-            # move currentIndex over
+            # Move currentIndex over:
+
             currentIndex += key
 
-    # Convert the ciphertext list into a single string value and return it.
+    # Convert the ciphertext list into a single string value and return it:
+
     return ''.join(ciphertext)
 
 
-# If transpositionEncrypt.py is run (instead of imported as a module) call
-# the main() function.
+# Call the main function:
+
 if __name__ == '__main__':
     main()
