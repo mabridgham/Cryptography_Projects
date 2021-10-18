afecab9c6a229a5e8b227d8f60299a5f943b4f61
unknown <bridgham.ma@gmail.com>
2021-10-18 15:53:21 -0700

    added description and formatting

--- transpositionTestMB.py
+++ transpositionTestMB.py
@@ -1,31 +1,41 @@
 # Transposition Cipher Test
-# https://www.nostarch.com/crackingcodes (BSD Licensed)
+# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+# A Transposition Cipher is a cipher in which the letters of the plaintext
+# are systematically rearranged into another sequence.
+
+# Test to see if the transposition cipher works and is valid.
 
 import random, sys, transpositionEncryptMB, transpositionDecryptMB
 
 def main():
-    random.seed(42) # set the random "seed" to a static value
 
-    for i in range(20): # run 20 tests
+    random.seed(42) # Set the random "seed" to a static value.
+
+    for i in range(20): # Run 20 tests.
+
         # Generate random messages to test.
 
         # The message will have a random length:
+
         message = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' * random.randint(4, 40)
 
-        # Convert the message string to a list to shuffle it.
+        # Convert the message string to a list to shuffle it:
+
         message = list(message)
         random.shuffle(message)
-        message = ''.join(message) # convert list to string
+        message = ''.join(message) # Convert the list back to a string.
 
-        print('Test #%s: "%s..."' % (i+1, message[:50]))
+        print('Test #%s: "%s..."' % (i + 1, message[:50]))
 
-        # Check all possible keys for each message.
-        for key in range(1, int(len(message) / 2)):
+        # Check all possible keys for each message:
+
+        for key in range(1, int(len(message)/2)):
             encrypted = transpositionEncryptMB.encryptMessage(key, message)
             decrypted = transpositionDecryptMB.decryptMessage(key, encrypted)
 
             # If the decryption doesn't match the original message, display
-            # an error message and quit.
+            # an error message and quit:
+
             if message != decrypted:
                 print('Mismatch with key %s and message %s.' % (key, message))
                 print('Decrypted as: ' + decrypted)
