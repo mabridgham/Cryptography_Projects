c8a6788cd0aa421692348dea9af6f77d487a69e4
unknown <bridgham.ma@gmail.com>
2021-10-18 09:21:08 -0700

    formatting

--- detectEnglishMB.py
+++ detectEnglishMB.py
@@ -2,15 +2,17 @@
 # https://www.nostarch.com/crackingcodes (BSD Licensed)
 
 # To use, type this code:
-#   import detectEnglish
-#   detectEnglish.isEnglish(someString) # returns True or False
-# (There must be a "dictionary.txt" file in this directory with all English
-# words in it, one word per line. You can download this from
-# https://invpy.com/dictionary.txt)
+# import detectEnglish
+# detectEnglish.isEnglish(someString) # Returns True or False
+# (There must be a "dictionary.txt" file in this directory with all
+# English words in it, one word per line. You can download this file from
+# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+
 UPPERLETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 LETTERS_AND_SPACE = UPPERLETTERS + UPPERLETTERS.lower() + ' \t\n'
 
 def loadDictionary():
+    
     dictionaryFile = open('dictionary.txt')
     englishWords = {}
     for word in dictionaryFile.read().split('\n'):
@@ -22,6 +24,7 @@ ENGLISH_WORDS = loadDictionary()
 
 
 def getEnglishCount(message):
+    
     message = message.upper()
     message = removeNonLetters(message)
     possibleWords = message.split()
@@ -37,6 +40,7 @@ def getEnglishCount(message):
 
 
 def removeNonLetters(message):
+    
     lettersOnly = []
     for symbol in message:
         if symbol in LETTERS_AND_SPACE:
@@ -45,11 +49,14 @@ def removeNonLetters(message):
 
 
 def isEnglish(message, wordPercentage=20, letterPercentage=85):
+    
     # By default, 20% of the words must exist in the dictionary file, and
     # 85% of all the characters in the message must be letters or spaces
     # (not punctuation or numbers).
+    
     wordsMatch = getEnglishCount(message) * 100 >= wordPercentage
     numLetters = len(removeNonLetters(message))
     messageLettersPercentage = float(numLetters) / len(message) * 100
     lettersMatch = messageLettersPercentage >= letterPercentage
+    
     return wordsMatch and lettersMatch
