a4df69113cfd02985fbda34f7fcc4197d9ee9824
unknown <bridgham.ma@gmail.com>
2021-10-18 11:22:05 -0700

    added description

--- simpleSubCipherMB.py
+++ simpleSubCipherMB.py
@@ -1,15 +1,18 @@
 # Simple Substitution Cipher
-# https://www.nostarch.com/crackingcodes (BSD Licensed)
+# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+# The Substitution Cipher is a cipher in which the letters of the plaintext
+# are systematically replaced by substitute letters.
 
 import pyperclip, sys, random
 
-
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
+
 def main():
-    myMessage = 'If a man is offered a fact which goes against his instincts, he will scrutinize it closely, and unless the evidence is overwhelming, he will refuse to believe it. If, on the other hand, he is offered something which affords a reason for acting in accordance to his instincts, he will accept it even on the slightest evidence. The origin of myths is explained in this way. -Bertrand Russell'
+
+    myMessage = 'If a man is offered a fact which goes against his instincts, he will scrutinize it closel_, and unless the evidence is overwhelming, he will refuse to _elieve it. If, on the other hand, he is offered something which affords a reason for acting in accordance to his instincts, he will acce_t it even on the slightest evidence. The origin of m_ths is e__lained in this wa_. -_ertrand Russell'
     myKey = 'LFWOAYUISVKMNXPBDCRJTQEGHZ'
-    myMode = 'encrypt' # Set to either 'encrypt' or 'decrypt'.
+    myMode = 'encrypt' # Set to 'encrypt' or 'decrypt'.
 
     if not keyIsValid(myKey):
         sys.exit('There is an error in the key or symbol set.')
@@ -17,6 +20,7 @@ def main():
         translated = encryptMessage(myKey, myMessage)
     elif myMode == 'decrypt':
         translated = decryptMessage(myKey, myMessage)
+
     print('Using key %s' % (myKey))
     print('The %sed message is:' % (myMode))
     print(translated)
@@ -26,6 +30,7 @@ def main():
 
 
 def keyIsValid(key):
+
     keyList = list(key)
     lettersList = list(LETTERS)
     keyList.sort()
@@ -35,39 +40,51 @@ def keyIsValid(key):
 
 
 def encryptMessage(key, message):
+
     return translateMessage(key, message, 'encrypt')
 
 
 def decryptMessage(key, message):
+
     return translateMessage(key, message, 'decrypt')
 
 
 def translateMessage(key, message, mode):
+
     translated = ''
     charsA = LETTERS
     charsB = key
+
     if mode == 'decrypt':
+
         # For decrypting, we can use the same code as encrypting. We
         # just need to swap where the key and LETTERS strings are used.
+
         charsA, charsB = charsB, charsA
 
-    # Loop through each symbol in message:
+    # Loop through each symbol in the message:
+
     for symbol in message:
         if symbol.upper() in charsA:
+
             # Encrypt/decrypt the symbol:
+
             symIndex = charsA.find(symbol.upper())
             if symbol.isupper():
                 translated += charsB[symIndex].upper()
             else:
                 translated += charsB[symIndex].lower()
         else:
-            # Symbol is not in LETTERS; just add it
+
+            # Symbol is not in LETTERS; just add it:
+            
             translated += symbol
 
     return translated
 
 
 def getRandomKey():
+
     key = list(LETTERS)
     random.shuffle(key)
     return ''.join(key)
