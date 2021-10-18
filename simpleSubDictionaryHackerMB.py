65152ff164752900bdba1f93168d266fce668fae
unknown <bridgham.ma@gmail.com>
2021-10-18 14:11:36 -0700

    added description and formatting

--- simpleSubDictionaryHackerMB.py
+++ simpleSubDictionaryHackerMB.py
@@ -1,34 +1,45 @@
 # Simple Substitution Dictionary Hacker
 # https://inventwithpython.com/hacking (BSD Licensed)
+# The Substitution Cipher is a cipher in which the letters of the plaintext
+# are systematically replaced by substitute letters.
 
-import pyperclip, simpleSubKeywordMB, detectEnglishMB
+import pyperclip, simpleSubKeywordMB , detectEnglishMB
 
 SILENT_MODE = False
 
 def main():
-    myMessage = r"""SJITDOPIQR: JIR RIQMUNQRO AY P WDQC QCR NRSMRQN JT A SJITDORO QJ CRMNRGT AY S. -PHAMJNR ADRMSR"""
+    
+    myMessage = """SJITDOPIQR: JIR RIQMUNQRO AY P WDQC QCR NRSMRQN JT A SJITDORO QJ CRMNRGT AY S. -PHAMJNR ADRMSR"""
 
     brokenCiphertext = hackSimpleSubDictionary(myMessage)
 
     if brokenCiphertext == None:
+        
         # hackSimpleSubDictionary() will return the None value if it was unable to hack the encryption.
+        
         print('Hacking failed. Unable to hack this ciphertext.')
+        
     else:
+        
         # The plaintext is displayed on the screen. For the convenience of the user, we copy the text of the code to the clipboard.
+        
         print('Copying broken ciphertext to clipboard:')
         print(brokenCiphertext)
         pyperclip.copy(brokenCiphertext)
 
 
 def hackSimpleSubDictionary(message):
+    
     print('Hacking with %s possible dictionary words...' % (len(detectEnglishMB.ENGLISH_WORDS) * 3))
 
     # Python programs can be stopped at any time by pressing Ctrl-C (on Windows) or Ctrl-D (on Mac and Linux)
+
     print('(Press Ctrl-C or Ctrl-D to quit at any time.)')
 
     tryNum = 1
 
-    # brute-force by looping through every possible key
+    # Brute-force by looping through every possible key
+
     for key in detectEnglishMB.ENGLISH_WORDS:
         if tryNum % 100 == 0 and not SILENT_MODE:
             print('%s keys tried. (%s)' % (tryNum, key))
@@ -36,7 +47,9 @@ def hackSimpleSubDictionary(message):
         decryptedText = simpleSubKeywordMB.decryptMessage(key, message)
 
         if detectEnglishMB.getEnglishCount(decryptedText) > 0.20:
+
             # Check with the user to see if the decrypted key has been found.
+
             print()
             print('Possible encryption hack:')
             print('Key: ' + str(key))
@@ -49,6 +62,7 @@ def hackSimpleSubDictionary(message):
                 return decryptedText
 
         tryNum += 1
+
     return None
 
 if __name__ == '__main__':
