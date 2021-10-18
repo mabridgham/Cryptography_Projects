a845fef888f45910381b9a995db7e15362d4fb3f
unknown <bridgham.ma@gmail.com>
2021-10-18 15:39:52 -0700

    added description and formatting

--- transpositionFileHackerMB.py
+++ transpositionFileHackerMB.py
@@ -1,5 +1,10 @@
 # Transposition File Hacker
 # https://www.nostarch.com/crackingcodes (BSD Licensed)
+# A Transposition Cipher is a cipher in which the letters of the plaintext
+# are systematically rearranged into another sequence.
+
+# To use the files created in transpositionFileCipherMB.py must be in the
+# same folder.
 
 import time, os, sys, transpositionDecryptMB, detectEnglishMB
 
@@ -7,6 +12,7 @@ inputFilename = 'frankenstein.encrypted.txt'
 outputFilename = 'frankenstein.decrypted.txt'
 
 def main():
+    
     if not os.path.exists(inputFilename):
         print('The file %s does not exist. Quitting.' % (inputFilename))
         sys.exit()
@@ -29,10 +35,15 @@ def main():
 
 # The hackTransposition() function's code was copy/pasted from
 # transpositionHacker.py and had some modifications made.
+
+
 def hackTransposition(message):
+    
     print('Hacking...')
+    
     # Python programs can be stopped at any time by pressing Ctrl-C (on
     # Windows) or Ctrl-D (on Mac and Linux)
+    
     print('(Press Ctrl-C or Ctrl-D to quit at any time.)')
 
     for key in range(1, len(message)):
@@ -41,6 +52,7 @@ def hackTransposition(message):
 
         # We want to track the amount of time it takes to test a single key,
         # so we record the time in startTime.
+        
         startTime = time.time()
 
         decryptedText = transpositionDecryptMB.decryptMessage(key, message)
@@ -59,6 +71,7 @@ def hackTransposition(message):
             response = input('> ')
             if response.strip().upper().startswith('D'):
                 return decryptedText
+            
     return None
 
 
