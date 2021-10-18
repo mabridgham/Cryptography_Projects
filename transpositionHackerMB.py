65ea8ed693f9228a845bf46e571da64a713890c1
unknown <bridgham.ma@gmail.com>
2021-10-18 15:26:29 -0700

    added description and formatting

--- transpositionHackerMB.py
+++ transpositionHackerMB.py
@@ -1,10 +1,12 @@
 # Transposition Cipher Hacker
-# https://www.nostarch.com/crackingcodes (BSD Licensed)
+# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+# A Transposition Cipher is a cipher in which the letters of the plaintext
+# are systematically rearranged into another sequence.
 
 import pyperclip, detectEnglishMB, transpositionDecryptMB
 
 def main():
-    
+
     myMessage = """AaKoosoeDe5 b5sn ma reno ora'lhlrrceey e  enlh na  indeit n uhoretrm au ieu v er Ne2 gmanw,forwnlbsya apor tE.no euarisfatt  e mealefedhsppmgAnlnoe(c -or)alat r lw o eb  nglom,Ain one dtes ilhetcdba. t tg eturmudg,tfl1e1 v  nitiaicynhrCsaemie-sp ncgHt nie cetrgmnoa yc r,ieaa  toesa- e a0m82e1w shcnth  ekh gaecnpeutaaieetgn iodhso d ro hAe snrsfcegrt NCsLc b17m8aEheideikfr aBercaeu thllnrshicwsg etriebruaisss  d iorr."""
 
     hackedMessage = hackTransposition(myMessage)
@@ -20,25 +22,29 @@ def main():
 def hackTransposition(message):
     print('Hacking...')
 
-    # Python programs can be stopped at any time by pressing Ctrl-C (on
-    # Windows) or Ctrl-D (on Mac and Linux)
-    print('(Press Ctrl-C or Ctrl-D to quit at any time.)')
+    # Python programs can be stopped at any time by pressing
+    # Ctrl-C (on Windows) or Ctrl-D (on macOS and Linux):
 
+    print('(Press Ctrl-C (on Windows) or Ctrl-D (on macOS and Linux) to quit at any time.)')
 
-    # Brute-force by looping through every possible key.
+    # Brute-force by looping through every possible
+
     for key in range(1, len(message)):
         print('Trying key #%s...' % (key))
 
         decryptedText = transpositionDecryptMB.decryptMessage(key, message)
 
         if detectEnglishMB.isEnglish(decryptedText):
-            # Ask user if this is the correct decryption.
+
+            # Ask user if this is the correct decryption:
+
             print()
             print('Possible encryption hack:')
             print('Key %s: %s' % (key, decryptedText[:100]))
             print()
             print('Enter D if done, anything else to continue hacking:')
             response = input('> ')
+
 
             if response.strip().upper().startswith('D'):
                 return decryptedText
