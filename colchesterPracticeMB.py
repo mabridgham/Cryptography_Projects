3f18f1e34addadb538ec42466408e5c49317513f
unknown <bridgham.ma@gmail.com>
2021-10-19 14:39:10 -0700

    added description and formatting

--- colchesterPracticeMB.py
+++ colchesterPracticeMB.py
@@ -1,32 +1,47 @@
-"""Solve a null cipher based on every nth-letter in every nth-word."""
+# Solve a null cipher based on every nth-letter in every nth-word.
+# https://www.nostarch.com/impracticalpythonprojects (BSD Licensed)
+# Takes an input, n, and checks for and displays a null cipher based
+# on the nth letter after the start of every nth word.
+
+# To see how this program would work, use the file colchester_message.txt
+# which has is available here for your convenience.
+
 import sys
 
 def load_text(file):
-    """Load a text file as a string."""
+    
+    # Load a text file as a string.
+    
     with open(file) as f:
         return f.read().strip()
 
-# load & process message:
+# Load & process message:
+
 filename = input("\nEnter full filename for message to translate: ")
+
 try:
     loaded_message = load_text(filename)
 except IOError as e:
     print("{}. Terminating program.".format(e), file=sys.stderr)
     sys.exit(1)
 
-# check loaded message & # of lines
+# Check loaded message & # of lines
+
 print("\nORIGINAL MESSAGE = {}\n".format(loaded_message))
 
-# convert message to list and get length
+# Convert message to list and get length
+
 message = loaded_message.split()
 end = len(message)
 
-# get user input on interval to check
+# Get user input on interval to check
+
 increment = int(input("Input max word & letter position to \
 check (e.g., every 1 of 1, 2 of 2, etc.): "))
 print()
 
-# find letters at designated intervals               
+# Find letters at designated intervals
+
 for i in range(1, increment + 1):
     print("\nUsing increment letter {} of word {}".format(i, i))
     print()
