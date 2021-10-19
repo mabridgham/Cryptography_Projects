bd7d31c3738f5f079acf51e3c0fe4725f40770c3
unknown <bridgham.ma@gmail.com>
2021-10-19 14:27:29 -0700

    corrected typos in print statement

--- onetimePadGenerateMB.py
+++ onetimePadGenerateMB.py
@@ -11,15 +11,17 @@ import detectEnglishMB, secrets
 
 print('''This program generates a one time pad that can be used as a key in the
 Vigenere Cipher. For this to truly qualify as a one time pad however it must meet
-the following qualifications:\n
-1. It is exactly as long as the encrypted message.\n
-2. It is made up of truly random symbols.\n
-3. It is used only once and never again for any other message.\n
+the following qualifications:
+
+1. It is exactly as long as the encrypted message.
+2. It is made up of truly random symbols.
+3. It is used only once and never again for any other message.
+
 This program allows the user to input the message they intend to use in their
 cipher and does not store it. It then uses that to generate a unique one time pad
 each time this program is run, even if the same original message is used to
 generate it. Once generated, simply copy and paste into your Vigenere Cipher to
-use as your key once and only once to make your cipher unbreakable.''')
+use as your key once and only once to make your cipher unbreakable.\n''')
 
 
 UPPERLETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
