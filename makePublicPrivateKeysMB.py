d029a1133f995c156e6cd951e885f5db521227dd
unknown <bridgham.ma@gmail.com>
2021-10-19 16:24:20 -0700

    added description and formatting

--- makePublicPrivateKeysMB.py
+++ makePublicPrivateKeysMB.py
@@ -1,37 +1,50 @@
 # Public Key Generator
 # https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+# The Public Key algorithm creates two large random prime numbers and
+# the uses complicated math (including finding a modular inverse) to create
+# the public and private keys.
 
 import random, sys, os, primeNumMB, cryptoMathMB
 
 
 def main():
-    # Create a public/private keypair with 1024 bit keys:
+
+    # Create a public/private keypair with 1024-bit keys:
+
     print('Making key files...')
     makeKeyFiles('IT_bee', 1024)
     print('Key files made.')
 
+
 def generateKey(keySize):
-    # Creates a public/private keys keySize bits in size.
+
+    # Creates public/private keys keySize bits in size.
+
     p = 0
     q = 0
-    # Step 1: Create two prime numbers, p and q. Calculate n = p * q.
-    print('Generating p & q primes...')
+
+    # Step 1: Create two prime numbers, p and q. Calculate n = p * q:
+
+    print('Generating p prime...')
     while p == q:
         p = primeNumMB.generateLargePrime(keySize)
         q = primeNumMB.generateLargePrime(keySize)
+
     n = p * q
 
-    # Step 2: Create a number e that is relatively prime to (p-1)*(q-1):
-    print('Generating e that is relatively prime to (p-1)*(q-1)...')
+    # Step 2: Create a number e that is relatively prime to (p - 1) * (q - 1):
+
+    print('Generating e that is relatively prime to (p - 1) * (q - 1)...')
     while True:
         # Keep trying random numbers for e until one is valid:
-        e = random.randrange(2 ** (keySize - 1), 2 ** (keySize))
-        if cryptoMathMB.gcd(e, (p - 1) * (q - 1)) == 1:
+        e = random.randrange(2**(keySize-1), 2**(keySize))
+        if cryptoMathMB.gcd(e, (p-1)*(q-1) == 1):
             break
 
     # Step 3: Calculate d, the mod inverse of e:
+
     print('Calculating d that is mod inverse of e...')
-    d = cryptoMathMB.findModInverse(e, (p - 1) * (q - 1))
+    d = cryptoMathMB.findModInverse(e, (p-1)*(q-1))
 
     publicKey = (n, e)
     privateKey = (n, d)
@@ -43,13 +56,15 @@ def generateKey(keySize):
 
 
 def makeKeyFiles(name, keySize):
+
     # Creates two files 'x_pubkey.txt' and 'x_privkey.txt' (where x
-    # is the value in name) with the n,e and d,e integers written in
+    # is the value in name) with the n, e and d, e integers written in
     # them, delimited by a comma.
 
     # Our safety check will prevent us from overwriting our old key files:
+
     if os.path.exists('%s_pubkey.txt' % (name)) or os.path.exists('%s_privkey.txt' % (name)):
-        sys.exit('WARNING: The file %s_pubkey.txt or %s_privkey.txt already exists! Use a different name or delete these files and re-run this program.' % (name, name))
+        sys.exit('WARNING: The file %s_pubkey.txt or %s_privkey.txt already exists! Use a different name or delete these files and rerun this program.' % (name, name))
 
     publicKey, privateKey = generateKey(keySize)
 
@@ -61,13 +76,13 @@ def makeKeyFiles(name, keySize):
     fo.close()
 
     print()
-    print('The private key is a %s and a %s digit number.' % (len(str(publicKey[0])), len(str(publicKey[1]))))
+    print('The private key is a %s and a %s digit number.' % (len(str(privateKey[0])), len(str(privateKey[1]))))
     print('Writing private key to file %s_privkey.txt...' % (name))
     fo = open('%s_privkey.txt' % (name), 'w')
     fo.write('%s,%s,%s' % (keySize, privateKey[0], privateKey[1]))
     fo.close()
 
 
-
 if __name__ == '__main__':
     main()
+                            
